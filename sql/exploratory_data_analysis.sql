19b22f291872d7af68f8d1b55d9d26628b209bba
MariiaDuk <mariyadukarets@gmail.com>
2025-05-12 14:11:23 +0300

    Update exploratory_data_analysis.sql

--- sql/exploratory_data_analysis.sql
+++ sql/exploratory_data_analysis.sql
@@ -1,10 +1,26 @@
 -- Дивимось, що є в таблиці та як це виглядає
+
 SELECT *
 FROM sales
 LIMIT 1000;
 
 
+-- Перейменовуємо стовпчик Date на TransactionDate, тому що Date — це зарезервоване слово, це може спричинити помилки
+
+CREATE OR REPLACE TABLE `my-pet-project-458715.Sales_Transaction.sales_transaction_original` AS
+SELECT TransactionNo
+  , `Date` AS TransactionDate
+  , ProductNo
+  , ProductName
+  , Price
+  , Quantity
+  , CustomerNo
+  , Country
+FROM `my-pet-project-458715.Sales_Transaction.sales_transaction_original`;
+
+
 -- Рахуємо загальну кількість рядків (536350) та кількість пустих значень по кожному стовпцю (відсутні)
+
 SELECT COUNT(*)
  , COUNT(*) - COUNT(TransactionNo)
  , COUNT(*) - COUNT(TransactionDate)
@@ -18,6 +34,7 @@ FROM sales;
 
 
 -- Рахуємо кількість null значень по кожному стовпцю (відсутні)
+
 SELECT COUNTIF(TransactionNo IS NULL)
  , COUNTIF(TransactionDate IS NULL)
  , COUNTIF(ProductNo IS NULL)
@@ -30,24 +47,28 @@ FROM sales;
 
 
 -- В описі датасета сказано, що у рядків з поверненнями TransactionNo починається з 'С', а Quantity має відʼємне значення. Перевіримо, чи є рядки, де кількість товару відʼємна, а номер транзакції не починається з 'С' (ні)
+
 SELECT *
 FROM sales
 WHERE Quantity < 0 AND LEFT(TransactionNo, 1) != 'C';
 
 
 -- Перевіримо, чи є рядки, де кількість товару додатна, а номер транзакції починається з 'С' (ні)
+
 SELECT *
 FROM sales
 WHERE Quantity > 0 AND LEFT(TransactionNo, 1) = 'C';
 
 
 -- Також перевіримо, чи є рядки, де кількість товару або вартість дорівнюють 0 (ні)
+
 SELECT *
 FROM sales
 WHERE Quantity = 0 OR Price = 0;
 
 
 -- Перевіряємо, чи є в таблиці дублікати (так, бачимо 4794 рядки, кожен з яких є в таблиці більше, ніж 1 раз)
+
 SELECT *, COUNT(*) AS cnt_rows
 FROM sales
 GROUP BY TransactionNo, TransactionDate, ProductNo, ProductName, Price, Quantity, CustomerNo, Country
@@ -55,6 +76,7 @@ HAVING COUNT(*) > 1;
 
 
 -- Ми будемо видаляти дублікати та зберігати результат як нову таблицю, з якою далі будемо працювати в табло. Щоб потім впевнитися, що цей етап пройшов нормально, порахуємо кількість унікальних рядків (526356) та додамо кількість рядків, які дублюються 526356+4794=531150. Тобто у новій таблиці ми маємо отримати 531150 рядків
+
 SELECT *, COUNT(*) AS cnt_rows
 FROM sales
 GROUP BY TransactionNo, TransactionDate, ProductNo, ProductName, Price, Quantity, CustomerNo, Country
