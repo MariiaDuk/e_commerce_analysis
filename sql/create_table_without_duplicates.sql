6869d695458f4a5413b1fd662484429a53449ee5
MariiaDuk <mariyadukarets@gmail.com>
2025-05-12 15:08:33 +0300

    Update create_table_without_duplicates.sql

--- sql/create_table_without_duplicates.sql
+++ sql/create_table_without_duplicates.sql
@@ -4,6 +4,7 @@ CREATE OR REPLACE TABLE sales_clean AS
 SELECT DISTINCT *
 FROM sales;
 
+
 -- Перевіряємо, чи отримали очікувану кількість рядків після очищення даних від дублікатів рядків (так, рядків 531150, як і очікувалось)
 
 SELECT COUNT(*)
